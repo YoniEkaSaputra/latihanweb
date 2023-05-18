e63afb58dfe53e609698fcc35fab0ce66665a39a
YoniEkaSaputra <yoniekasaputra17@gmail.com>
2023-05-18 14:03:49 +0700

    jwt controller

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
-
+ 
 use Illuminate\Http\Request;
-
+ 
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -12,7 +12,21 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+ 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+   return $request->user();
 });
+ 
+Route::group([
+ 
+    'middleware' => ['api','jwt.verify'],
+ 
+], function ($router) {
+ 
+   Route::post('logout', 'AuthController@logout');
+   Route::post('refresh', 'AuthController@refresh');
+   Route::post('me', 'AuthController@me');
+});
+   Route::post('users', 'UserController@store');
+   Route::get('users', 'UserController@index');
+   Route::post('login', 'AuthController@login');
